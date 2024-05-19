d6b5fc61881659a403c5a171121724e6a6360ad6
Sergio Wallace <way2mars@hotmail.com>
2024-05-19 20:03:34 +0700

    Initial commit

---
+++ app/src/main/java/ru/way2mars/howmuchforty/view/CounterView.kt
@@ -0,0 +1,13 @@
+package ru.way2mars.howmuchforty.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.cardview.widget.CardView
+
+
+class CounterView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : CardView(context, attrs, defStyleAttr) {
+}
---
+++ app/src/main/java/ru/way2mars/howmuchforty/view/MainActivity.kt
@@ -0,0 +1,35 @@
+package ru.way2mars.howmuchforty.view
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import android.view.Menu
+import ru.way2mars.howmuchforty.R
+import ru.way2mars.howmuchforty.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+}
